2f008c0ce13c76a7c5cbd8ec278da8c029cc8f3d
Bowen Slingluff <bcslingluff@gmail.com>
2024-06-18 11:17:17 -0400

    setting up database conection

---
+++ -- SQLite.sql
@@ -0,0 +1,20 @@
+-- SQLite
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT NOT NULL,
+  hash TEXT NOT NULL,
+  cash NUMERIC NOT NULL DEFAULT 50
+);
+
+CREATE UNIQUE INDEX username ON users (username);
+
+CREATE TABLE bets (
+  id INTEGER PRIMARY KEY,
+  userid INTEGER NOT NULL,
+  event TEXT NOT NULL, 
+  selection TEXT NOT NULL,
+  odds REAL NOT NULL,
+  amount NUMERIC NOT NULL,
+  timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY(userid) REFERENCES users(id)
+);
