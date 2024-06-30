98b03e09e2838afeb2704f3a66eb602a950cfb80
Bowen Slingluff <bcslingluff@gmail.com>
2024-06-30 18:50:13 -0400

    setting up site

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
-import re
+import os
 import sqlite3
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-from flask import Flask
-
-
-
+import helpers
 
 app = Flask(__name__)
-def get_db_connection():
-    conn = sqlite3.connect('bets.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+
+
+conn = sqlite3.connect('bets.db')
+conn.row_factory = sqlite3.Row
+cur = conn.cursor()
+
+
+
 
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+def index():
+    return render_template("index.html")
 
