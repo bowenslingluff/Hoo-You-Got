50302ac2302137df48d40c6109803c40520759fc
Bowen Slingluff <bcslingluff@gmail.com>
2024-07-01 15:47:56 -0400

    made database helper functions

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
 import os
 import sqlite3
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, g, flash, redirect, render_template, request, session, make_response
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
-import helpers
+from helpers import connect, query
 
 app = Flask(__name__)
+app.config['DATABASE'] = 'bets.db'
 
 
-conn = sqlite3.connect('bets.db')
-conn.row_factory = sqlite3.Row
-cur = conn.cursor()
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 
+db = connect()
+cur = db.cursor()
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+if __name__ == '__main__':
+    app.run(debug=True)
--- helpers.py
+++ helpers.py
@@ -1,2 +1,21 @@
+import sqlite3
+from flask import g, current_app
 
+def connect():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(current_app.config['DATABASE'])
+        db.row_factory = sqlite3.Row
+    return db
 
+def execute(query, args=(), one=False):
+    db = connect()
+    cur = db.execute(query, args)
+    if query.strip().upper().startswith('SELECT'):
+        rv = cur.fetchall()
+        cur.close()
+        return (rv[0] if rv else None) if one else rv
+    else:
+        db.commit()
+        cur.close()
+        return None
