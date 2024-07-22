26d5b224fe0f6d418a492fa753fc4d07582f0a54
Bowen Slingluff <bcslingluff@gmail.com>
2024-07-22 12:30:26 -0400

    added table for basketball bets

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import sqlite3
 from flask import Flask, g, flash, redirect, render_template, request, session
 from flask_session import Session
@@ -16,6 +17,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+API_KEY = '3fe51db060b5849e455f770a4b92b2ab'
+REGIONS = 'us'
+MARKETS = 'h2h,spreads,totals'
 
 @app.after_request
 def after_request(response):
@@ -33,9 +37,25 @@ def teardown_db(exception):
         db.close()
 
 @app.route("/")
+@login_required
 def index():
-    # username = execute("SELECT username FROM users WHERE id = ?", session.get("user_id"))
+    userid = session.get('userid')
+
     return render_template("index.html")
+
+@app.route("/basketball")
+@login_required
+def basketball():
+    SPORT = "basketball_nba"
+    url = f'https://api.the-odds-api.com/v4/sports/{SPORT}/odds/'
+    params = {
+        'apiKey': API_KEY,
+        'regions': REGIONS,
+        'markets': MARKETS
+    }
+    response = requests.get(url, params=params)
+    games = response.json()
+    return render_template("basketball.html", games=games)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -68,10 +88,10 @@ def register():
             execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
             user = query_db("SELECT id FROM users WHERE username = ?", (username,), one=True)
             session["user_id"] = user["id"]
-            return redirect("/")
+            return redirect("/login")
         except sqlite3.IntegrityError:
             flash("Username must be unique!")
-            return redirect("/register")
+            return redirect("/login")
     else:
         return render_template("register.html")
 
@@ -85,15 +105,11 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-        # Ensure username was submitted
-        if not request.form.get("username"):
-            return flash("All fields are required!")
-
-        # Ensure password was submitted
-        elif not request.form.get("password"):
-            return flash("All fields are required!")
-
         username = request.form.get("username")
+        password = request.form.get("password")
+        # Ensure username was submitted
+        if not username or not password:
+            return flash("All fields are required!")
 
         # Query database for username
         rows = query_db(
@@ -101,17 +117,14 @@ def login():
         )
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows["hash"], request.form.get("password")
-        ):
-            return flash("Invalid username or password!")
-
-        # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
-
-        # Redirect user to home page
-        return redirect("/")
-
+        if rows and check_password_hash(rows['hash'], password):
+            # Remember which user has logged in
+            session["user_id"] = rows["id"]
+            # Redirect to the home page
+            return redirect("/")
+        else:
+            flash("Invalid username or password")
+            return render_template("login.html")
     # User reached route via GET (as by clicking a link or via redirect)
     else:
         return render_template("login.html")
